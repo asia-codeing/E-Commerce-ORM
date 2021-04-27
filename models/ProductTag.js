f09969787a07d74a3daa2f7a25e1be6d032716e2
Asia Alnahi <asia.alius@gmail.com>
2021-04-27 16:54:58 -0700

    create columns in ProductTag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,3 +1,4 @@
+const { DATE } = require('sequelize');
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
@@ -6,7 +7,29 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+
+    },
+
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      }
+    },
+
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    },
   },
   {
     sequelize,
