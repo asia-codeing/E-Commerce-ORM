f74c5e5eb0c1826aba76a0e6e5362dca066b0a0f
Asia Alnahi <asia.alius@gmail.com>
2021-04-27 16:59:25 -0700

    create columns in Tag model

--- models/Tag.js
+++ models/Tag.js
@@ -1,3 +1,4 @@
+const { DATE } = require('sequelize');
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
@@ -6,7 +7,16 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
