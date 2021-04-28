9fc204406b49e008ff293889b7bb5ff8fb03b6d2
Asia Alnahi <asia.alius@gmail.com>
2021-04-28 13:50:38 -0700

    execute association methods on the models

--- models/index.js
+++ models/index.js
@@ -5,13 +5,31 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey:'category_id',
+  onDelete: 'CASCADE'
+});
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey:'category_id',
+  onDelete: 'CASCADE'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag
+  },
+  as: 'product_has_tag'
+});
 
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag
+  },
+  as: 'tag_of_product'
+});
 module.exports = {
   Product,
   Category,
