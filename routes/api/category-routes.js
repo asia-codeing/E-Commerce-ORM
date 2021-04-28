356256c7a4e961cfd35ca2dd6ea22f2f013717f2
Asia Alnahi <asia.alius@gmail.com>
2021-04-28 16:40:19 -0700

    working on the router for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,8 +3,20 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
+  try{
+    const categorieData = await Category.findAll({
+      include: 
+      {
+        model: Product
+      }
+    });
+    res.status(200).json(categorieData);
+  } catch (err) {
+    res.status(500).json(err);
+
+  }
   // be sure to include its associated Products
 });
 
