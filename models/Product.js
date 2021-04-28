0ebe65aa5f5f4cfdf1d2cdd2542845086a5f5c8b
Asia Alnahi <asia.alius@gmail.com>
2021-04-27 17:08:30 -0700

    Added some columns code in Product model

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,5 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, DECIMAL } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -9,7 +9,52 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false
+      // Decimal.
+      
+      // Doesn't allow null values.
+      
+      // Validates that the value is a decimal.
+    },
+
+    stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 10,
+
+      // Integer.
+      
+      // Doesn't allow null values.
+      
+      // Set a default value of 10.
+      
+      // Validates that the value is numeric.
+    },
+
+    category_id: {
+      
+
+      // Integer.
+      
+      // References the Category model's id.
+    }
+
+
+
   },
   {
     sequelize,
