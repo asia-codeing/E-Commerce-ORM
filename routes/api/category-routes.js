4b7e70b4d138770ba7cd03dbf3dcc84160b65dd7
Asia Alnahi <asia.alius@gmail.com>
2021-04-29 13:37:44 -0700

    Filling out tag routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -54,7 +54,9 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
-    const categoryData = await Category.update(req.body, 
+    const categoryData = await Category.update({
+      id:req.body.id
+    }, 
       {
       where: {
          id: req.params.id 
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,9 +9,13 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Category and Tag data
 try {
   const productData = await Product.findAll({
-    include: {
+    include: [{
       model: Category
+    },
+    {
+      model:Tag
     }
+  ]
   });
   res.status(200).json(productData);
 } catch (err) {
@@ -26,10 +30,13 @@ router.get('/:id',  async (req, res) => {
   // be sure to include its associated Category and Tag data
 try {
   const productData = await Product.findByPk(req.params.id, {
-    include: {
-      model: Category,
-      through: ProductTag,
+    include: [{
+      model: Category
+    },
+    {
+      model:Tag
     }
+  ]
   });
   if (!productData) {
     res.status(404).json({ message: 'Their is no product with this id'});
@@ -83,7 +90,12 @@ router.put('/:id', (req, res) => {
   })
     .then((product) => {
       // find all associated tags from ProductTag
-      return ProductTag.findAll({ where: { product_id: req.params.id } });
+      return ProductTag.findAll({ 
+          where: 
+          { 
+            product_id: req.params.id 
+          } 
+        });
     })
     .then((productTags) => {
       // get list of current tag_ids
