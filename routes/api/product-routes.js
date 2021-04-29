c9e47f958541642a052cc38ce79c8b52687d2b8e
Asia Alnahi <asia.alius@gmail.com>
2021-04-29 12:55:25 -0700

    Fill out proudect routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -115,8 +115,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id 
+      }
+    });
+    if (!productData) {
+      res.status(404).json({message: 'No Product found'});
+      return;
+    }
+  res.status(200).json(productData);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
 module.exports = router;
