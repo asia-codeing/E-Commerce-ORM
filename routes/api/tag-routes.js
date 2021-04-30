55aa2f159cb10a2b64a2ddcaf708fb2545f774fc
Asia Alnahi <asia.alius@gmail.com>
2021-04-30 11:39:49 -0700

    Filling out tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -73,6 +73,23 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({ 
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No Tag found with this id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
 
 module.exports = router;
