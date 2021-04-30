44d0eeddddc49ddbe47529462fd31297793c0082
Asia Alnahi <asia.alius@gmail.com>
2021-04-30 11:37:40 -0700

    Filling out tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -37,12 +37,38 @@ try{
 }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+
+  } catch (err) {
+    res.status(400).json(err);
+
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update({
+      id: req.body.id  
+    }, 
+    {
+      where: {
+        id: req.params.id 
+       },
+     returning: true
+      }
+    );
+    if (!tagData) 
+    const data = tagData[1][0].get();
+    res.status(200).json({ success: true, data });
+  } catch (err) {
+    res.status(500).json(err);
+
+  }
 });
 
 router.delete('/:id', (req, res) => {
